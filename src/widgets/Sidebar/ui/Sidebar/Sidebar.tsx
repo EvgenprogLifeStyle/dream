e02c0a894beb8084371e80e197e425453d585f48
EvgenprogLifeStyle <stworkout83@gmail.com>
2023-02-12 16:56:56 +0300

    add sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,21 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import s from './Sidebar.module.scss' 
+import { useState } from 'react';
+
+interface NavbarProps {
+    className?: string
+}
+
+export const Sidebar = ({className}: NavbarProps) => {
+
+    const [collapsed, setCollapsed] = useState(false)
+    return (
+        <div className={classNames(s.Sidebar, {[s.collapsed]: collapsed }, [className])}>
+          
+            <div className={s.links}>
+            
+            </div>
+
+        </div>
+    );
+};
